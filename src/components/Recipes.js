a74f6fd7f5b0053bc14ff1d306c149716881b008
wwwdrt <juandapps@hotmail.com>
2023-06-03 20:46:00 -0500

    Update recipes component

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,47 +1,44 @@
-class Recipes {
-  constructor() {
-    this.render = document.getElementById('cards');
-    this.url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=chicken';
-    this.recipes = [];
+const render = document.getElementById('cards');
+const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=chicken';
+const recipes = [];
+
+const fetchRecipes = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  const chickenMeal = data.meals;
+
+  for (let i = 0; i < 6; i += 1) {
+    const recipe = {
+      id: chickenMeal[i].idMeal,
+      image: chickenMeal[i].strMealThumb,
+      name: chickenMeal[i].strMeal,
+    };
+    recipes.push(recipe);
   }
+};
 
-  fetch = async () => {
-    const res = await fetch(this.url);
-    const data = await res.json();
-    const chickenMeal = data.meals;
+const displayCards = async () => {
+  recipes.forEach((meal) => {
+    const card = document.createElement('div');
+    card.classList.add('dish-element');
+    card.dataset.id = meal.id;
 
-    for (let i = 0; i < 6; i += 1) {
-      const recipe = {
-        id: chickenMeal[i].idMeal,
-        image: chickenMeal[i].strMealThumb,
-        name: chickenMeal[i].strMeal,
-      };
-      this.recipes.push(recipe);
-    }
-  }
+    card.innerHTML = `
+        <img class="chicken-image" src="${meal.image}" alt="">
+        <ul class="name-and-like">
+          <li class="dish-name">${meal.name}</li>
+          <li class="material-icons like-icon">favorite</li>
+        </ul>
+        <p>2 likes</p>
+        <div class="comments-and-reservations">
+          <a href="#"><button type="submit" class="btn-comments">Comments</button></a>
+          <a href="#"><button type="submit" class="btn-reservations">Reservations</button></a>
+        </div>
+      `;
 
-  displayCards = async () => {
-    this.recipes.forEach((meal) => {
-      const card = document.createElement('div');
-      card.classList.add('dish-element');
-      card.dataset.id = meal.id;
+    render.appendChild(card);
+  });
+};
 
-      card.innerHTML = `
-          <img class="chicken-image" src="${meal.image}" alt="">
-          <ul class="name-and-like">
-            <li class="dish-name">${meal.name}</li>
-            <li class="material-icons like-icon">favorite</li>
-          </ul>
-          <p>2 likes</p>
-          <div class="comments-and-reservations">
-            <a href="#"><button type="submit" class="btn-comments">Comments</button></a>
-            <a href="#"><button type="submit" class="btn-reservations">Reservations</button></a>
-          </div>
-        `;
+export { fetchRecipes,  displayCards };
 
-      this.render.appendChild(card);
-    });
-  }
-}
-
-export default Recipes;
